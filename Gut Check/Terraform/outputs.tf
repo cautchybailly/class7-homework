e63f7ced567db7c1c04e5661b51466dc1fabe523
Cautchy <104728544Cautchybailly@users.noreply.github.com>
2026-03-29 19:21:34 -0400

    outputs update

--- Gut Check/Terraform/outputs.tf
+++ Gut Check/Terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
-description = "Name of the created S3 bucket"
-value = var.bucket_name.id
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.class7_gut_check_bucket.id
 }
 
 output "bucket_arn" {
-description = "ARN of the created S3 bucket"
-value = var.bucket_name.arn
+  description = "ARN of the created S3 bucket"
+  value       = aws_s3_bucket.class7_gut_check_bucket.arn
 }
