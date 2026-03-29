e01aa482cce996fdb3596b9c7feef02adf6b198d
Cautchy <104728544Cautchybailly@users.noreply.github.com>
2026-03-29 19:19:17 -0400

    another one

--- Gut Check/Terraform/main.tf
+++ Gut Check/Terraform/main.tf
@@ -1,26 +1,24 @@
-## **main.tf**
-
 resource "aws_s3_bucket" "class7_gut_check_bucket" {
-bucket = var.bucket_name
+  bucket = var.bucket_name
 
-tags = {
-Name = "Class7-GutCheck"
-Environment = "Lab"
-ManagedBy = "Terraform"
-}
+  tags = {
+    Name        = "Class7-GutCheck"
+    Environment = "Lab"
+    ManagedBy   = "Terraform"
+  }
 }
 
 resource "aws_s3_bucket_ownership_controls" "ownership" {
-bucket = var.bucket_name.id
-rule {
-object_ownership = "BucketOwnerPreferred"
-}
+  bucket = aws_s3_bucket.class7_gut_check_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_object" "armageddon_clearance" {
-for_each = fileset("${path.module}/../screenshots/", "*.png")
+  for_each = fileset("${path.module}/../screenshots/", "*.png")
 
-bucket = var.bucket_name.id
-key = each.value
-source = "${path.module}/../screenshots/${each.value}"
+  bucket = aws_s3_bucket.class7_gut_check_bucket.id
+  key    = each.value
+  source = "${path.module}/../screenshots/${each.value}"
 }
