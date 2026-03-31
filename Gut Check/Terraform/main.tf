44d66ed99ce456a878c1d4196bdc69867bfe6921
Cautchy <104728544Cautchybailly@users.noreply.github.com>
2026-03-31 13:52:20 -0400

    added explicit code to make sure that the S3 bucket is public

--- Gut Check/Terraform/main.tf
+++ Gut Check/Terraform/main.tf
@@ -8,6 +8,15 @@ resource "aws_s3_bucket" "class7_gut_check_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.class7_gut_check_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.class7_gut_check_bucket.id
   rule {
